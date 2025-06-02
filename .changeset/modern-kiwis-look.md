d52e67004b117ab0df949eb0308535a715a09d2d
David Glasser <glasser@davidglasser.net>
2025-06-02 16:18:54 -0700

    Publish as ESM-only (#11)
    Require Node v20.19 (which enables require(esm) support by default).
    
    This new package is supposed to be compatible with Apollo Server 4 which
    does an elaborate dance to publish as both CJS and ESM, but we already
    decided it was OK for this package itself to require Node v20.
    
    Turns out CJS-only plays poorly with the AS4-style dual build:
    https://github.com/apollographql/apollo-server/issues/8069
    
    And we're likely to go ESM-only with AS5 because newer Node makes this
    easier. So let's do that here too.
    
    Making tests work requires:
    - Tweaking ts-jest to some new presets.
    - Passing an experimental option to Node (and disabling the warning
      about it).
    - Upgrading the integration test suite to a version that is dual-built
      instead of CJS-only (otherwise we end up with different ApolloServers
      from the `@apollo/server` we load directly (ESM) and via the
      integration test suite (CJS)).

---
+++ .changeset/modern-kiwis-look.md
@@ -0,0 +1,5 @@
+---
+'@as-integrations/express5': major
+---
+
+Publish as ESM-only, and require Node v20.19 (which enables require(esm) support by default).
--- src/__tests__/expressSpecific.test.ts
+++ src/__tests__/expressSpecific.test.ts
@@ -3,10 +3,9 @@ import request from 'supertest';
 import compression, { filter as defaultFilter } from 'compression';
 import type { BaseContext } from '@apollo/server';
 import { ApolloServer } from '@apollo/server';
-import { expressMiddleware } from '..';
+import { expressMiddleware } from '../../dist/index.js';
 import { it, expect } from '@jest/globals';
 import cors from 'cors';
-import { resolvable } from './utils';
 
 it('gives helpful error if json middleware is not installed', async () => {
   const server = new ApolloServer({ typeDefs: 'type Query {f: ID}' });
@@ -203,3 +202,36 @@ it('supporting doubly-encoded variables example from migration guide', async () 
 
   await server.stop();
 });
+
+// Copyright 2019 Joseph Gentle
+
+// Permission to use, copy, modify, and / or distribute this software for any
+// purpose with or without fee is hereby granted, provided that the above
+// copyright notice and this permission notice appear in all copies.
+
+// THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH
+// REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND
+// FITNESS.IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
+//   INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+// LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR
+// OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+// PERFORMANCE OF THIS SOFTWARE.
+
+export type Resolvable<T> = Promise<T> & {
+  resolve: (t: T) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  reject: (e: any) => void;
+};
+
+export function resolvable<T = void>(): Resolvable<T> {
+  let resolve: (val: T) => void;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let reject: (err: any) => void;
+  const promise = new Promise<T>((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  }) as Resolvable<T>;
+  promise.resolve = resolve!;
+  promise.reject = reject!;
+  return promise;
+}
